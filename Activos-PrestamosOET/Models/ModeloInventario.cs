7f9c189596f181d2456922edf1da6bde2cf9822d
helenaRratedSexy <hcubasv@gmail.com>
2016-04-20 10:08:39 -0600

    Inventario

---
+++ Activos-PrestamosOET/Models/ModeloInventario.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Inventario.Models
+{
+    public class ModeloInventario
+    {
+        //public Local.Models.Prestamo Prestamos { get; set; }
+        //public Local.Models.usuario Usuarios { get; set; }
+    }
+}
