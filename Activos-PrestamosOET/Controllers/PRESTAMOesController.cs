1e58e134c60ea74b6007a58c7a13c97f3669dcd3
andrealgomez95 <andreittttta@hotmail.com>
2016-04-20 18:48:31 -0600

    Detalles, Historial y Editar a medio hacer

--- Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
+++ Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
@@ -17,12 +17,24 @@ namespace Activos_PrestamosOET.Controllers
         public ActionResult Index()
         {
            return View(db.PRESTAMOS.ToList());
-           
-           
-           
-           
-        }       
-
+        }
+        public ActionResult Historial()
+        {
+            return View(db.PRESTAMOS.ToList());
+        }
+        public ActionResult Detalles(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRESTAMO pRESTAMO = db.PRESTAMOS.Find(id);
+            if (pRESTAMO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRESTAMO);
+        }
         // GET: PRESTAMOes/Details/5
         public ActionResult Details(string id)
         {
