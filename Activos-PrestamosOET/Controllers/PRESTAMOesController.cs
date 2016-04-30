fbdf35b0225792017abadfc4de28cfb689ddb6b4
andreagomez95 <andreittttta@gmail.com>
2016-04-30 09:51:45 -0600

    Comentarios

--- Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
+++ Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
@@ -1044,155 +1044,24 @@ namespace Activos_PrestamosOET.Controllers
 
             return View(P);
         }
-        /* // POST: PRESTAMOes/Edit/5
-         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-         [HttpPost]
-         [ValidateAntiForgeryToken]
-         public ActionResult Edit([Bind(Include = "ID,NUMERO_BOLETA,MOTIVO,FECHA_SOLICITUD,FECHA_RETIRO,PERIODO_USO,SOFTWARE_REQUERIDO,OBSERVACIONES_SOLICITANTE,OBSERVACIONES_APROBADO,OBSERVACIONES_RECIBIDO,CEDULA_USUARIO,SIGLA_CURSO")] PRESTAMO p, string id, int[] cantidad, string b)
-         {
-             PRESTAMO P = db.PRESTAMOS.Find(id);
-             var equipo_sol = from o in db.PRESTAMOS
-                              from o2 in db.EQUIPO_SOLICITADO
-                              where (o.ID == id && o2.ID_PRESTAMO == id)
-                              select new { ID = o.ID, ID_EQUIPO = o2.ID_PRESTAMO, TIPO = o2.TIPO_ACTIVO, CANTIDAD = o2.CANTIDAD, CANTAP = o2.CANTIDADAPROBADA };
-             var cat = (from ac in db.ACTIVOS
-                        from t in db.TIPOS_ACTIVOS
-                        where ac.PRESTABLE.Equals(true) && t.ID.Equals(ac.TIPO_ACTIVOID)
-                        select new { t.NOMBRE, t.ID }).Distinct();
-             cat = cat.OrderBy(t => t.NOMBRE);
-                 int a = 0;
-                 foreach (var y in cat)
-                 {
-                     bool noEsta = true;
-                     foreach (var x in equipo_sol)
-                     {
-                         if (y.ID.ToString() == x.TIPO)
-                         {
-                             EQUIPO_SOLICITADO pr = db.EQUIPO_SOLICITADO.Find(id, y.ID.ToString(), x.CANTIDAD);
-                             if (pr == null)
-                             {
-                                 pr = new EQUIPO_SOLICITADO();
-                                 pr.ID_PRESTAMO = id;
-                                 pr.TIPO_ACTIVO = y.ID.ToString();//traerCategoria(cat[a]); ;
-                                 pr.CANTIDAD = cantidad[a];
-                                 if (ModelState.IsValid)
-                                 {
-                                     db.EQUIPO_SOLICITADO.Add(pr);
-                                     db.SaveChanges();
-                                     Console.WriteLine('e');
-                                 }
-                                 else
-                                 {
-                                     Console.WriteLine('a');
-                                 }
-                             }
-                             else
-                             {
-                                 EQUIPO_SOLICITADO eq = new EQUIPO_SOLICITADO();
 
-                                 decimal temp = cantidad[a];
-                                 noEsta = false;
-                                 eq.ID_PRESTAMO = pr.ID_PRESTAMO;
-                                 eq.TIPO_ACTIVO = pr.TIPO_ACTIVO;
-                                 eq.CANTIDAD = temp;
-                                 eq.CANTIDADAPROBADA = pr.CANTIDADAPROBADA;
-                                 db.EQUIPO_SOLICITADO.Remove(pr);
-                             db.SaveChanges();
-                             if (ModelState.IsValid)
-                                 {
-                                     //db.Entry(pr).State = EntityState.Modified;
-                                     db.EQUIPO_SOLICITADO.Add(eq);
-                                     db.SaveChanges();
-                                     Console.WriteLine('f');
-                                 }
-                                 else
-                                 {
-                                     Console.WriteLine('b');
-                                 }
-                             }
-
-                         }
-                     }
-
-                     if (noEsta)
-                     {
-                         EQUIPO_SOLICITADO pr = new EQUIPO_SOLICITADO();
-                         pr.ID_PRESTAMO = id;
-                         pr.TIPO_ACTIVO = y.ID.ToString();
-                         pr.CANTIDAD = cantidad[a];
-                         Console.WriteLine('g');
-                         if (ModelState.IsValid)
-                         {
-                             db.EQUIPO_SOLICITADO.Add(pr);
-                             db.SaveChanges();
-                             Console.WriteLine('h');
-                         }
-                         else
-                         {
-                             Console.WriteLine('c');
-                         }
-                     }
-                     else
-                     {
-                         Console.WriteLine('d');
-                     }
-                     a++;
-
-                 }
-                 ViewBag.Mensaje = "El préstamo ha sido aprobado con éxito";
-
-             var lista = from o in db.PRESTAMOS
-                         from o2 in db.USUARIOS
-                         where o.ID == id
-                         select new { Prestamo = o, CEDULA = o2.IDUSUARIO, USUARIO = o2.NOMBRE };
-
-             foreach (var m in lista)
-             {
-                 if (m.Prestamo.ID == id)
-                 {
-                     if (m.Prestamo.CED_SOLICITA == m.CEDULA)
-                     {
-                         var t = new Tuple<string>(m.USUARIO);
-                         ViewBag.Nombre = t.Item1;
-                     }
-                 }
-             }
-
-             ViewBag.fechSol = P.FECHA_SOLICITUD.Value.ToShortDateString();
-             P.MOTIVO = p.MOTIVO;
-             P.OBSERVACIONES_SOLICITANTE = p.OBSERVACIONES_SOLICITANTE;
-             P.PERIODO_USO = p.PERIODO_USO;
-             P.SIGLA_CURSO = p.SIGLA_CURSO;
-             P.FECHA_RETIRO = p.FECHA_RETIRO;
-             P.SOFTWARE_REQUERIDO = "";
-             P.Estado = 1;
-             if (ModelState.IsValid)
-             {
-                 db.Entry(P).State = EntityState.Modified;
-                 db.SaveChanges();
-                 return RedirectToAction("Historial");
-             }
-
-             return View(P);
-         }
-         */
 
 
         // GET: PRESTAMOes/Delete/5
         public ActionResult Delete(string id)
         {
-
+            //Si no entra al cancelar de una solicitud en especifico da error
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             PRESTAMO pRESTAMO = db.PRESTAMOS.Find(id);
-
+            //Si entra a cancelar una solicitud que no existe, da error
             if (pRESTAMO == null)
             {
                 return HttpNotFound();
             }
+            //Para determinar el estado en que se encuentra la solicitud en este momento
             ViewBag.Estadillo = "";
             if (pRESTAMO.Estado == 1)
             {
@@ -1218,11 +1087,12 @@ namespace Activos_PrestamosOET.Controllers
             {
                 ViewBag.Estadillo = "Cancelada";
             }
+            //Para determinar cual usuario fue el que hizo la solicitud
             var lista = from o in db.PRESTAMOS
                         from o2 in db.USUARIOS
                         where o.ID == id
                         select new { Prestamo = o, CEDULA = o2.IDUSUARIO, USUARIO = o2.NOMBRE };
-
+            
             foreach (var m in lista)
             {
                 if (m.Prestamo.ID == id)
@@ -1234,20 +1104,20 @@ namespace Activos_PrestamosOET.Controllers
                     }
                 }
             }
-
+            //Para determinar las categorias de activos que existen
             var cat = (from ac in db.ACTIVOS
                        from t in db.TIPOS_ACTIVOS
                        where ac.PRESTABLE.Equals(true) &&
                               t.ID.Equals(ac.TIPO_ACTIVOID)
                        select new { t.NOMBRE, t.ID }).Distinct();
-
+            //Para desplegar las cantidades solicitadas de cada categoria por ese usuario en esa solicitud especifica
             var equipo_sol = from o in db.PRESTAMOS
                              from o2 in db.EQUIPO_SOLICITADO
                              where (o.ID == id && o2.ID_PRESTAMO == id)
                              select new { ID = o.ID, ID_EQUIPO = o2.ID_PRESTAMO, TIPO = o2.TIPO_ACTIVO, CANTIDAD = o2.CANTIDAD, CANTAP = o2.CANTIDADAPROBADA };
-            
+            //Para crear la lista de equipo con cantidades que se va a desplegar en la tabla de equipo solicitado que se 
+            //podra observar en la pantalla de cancelar (llamada Delete)
             var equipo = new List<List<String>>();
-
             foreach (var y in cat)
             {
                 bool existeCategoria = false;
@@ -1283,6 +1153,7 @@ namespace Activos_PrestamosOET.Controllers
                 }
                 equipo.Add(temp);
             }
+            //la variable en la que se guarda la informacion a desplegarse en la tabla de equipo solicitado de la pantalla de cancelacion
             ViewBag.Equipo_Solict = equipo;
             return View(pRESTAMO);
         }
@@ -1291,37 +1162,22 @@ namespace Activos_PrestamosOET.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
+            //busca el objeto PRESTAMO que tenga el id correspondiente
             PRESTAMO pRESTAMO = db.PRESTAMOS.Find(id);
+            //Pone el estado del prestamos seleccionado en 6 (cancelado)
             pRESTAMO.Estado = 6;
+            //Verifica que el modelo siga siendo valido despues del cambio
             if (ModelState.IsValid)
             {
+                //Pone el estado del objeto prestamo seleccionado en modificado
                 db.Entry(pRESTAMO).State = EntityState.Modified;
+                //guarda los cambios en la base
                 db.SaveChanges();
                 return RedirectToAction("Historial");
             }
             return RedirectToAction("Historial");
         }
-        /*  // POST: PRESTAMOes/Delete/5
-          [HttpPost, ActionName("Delete")]
-          [ValidateAntiForgeryToken]
-          public ActionResult DeleteConfirmed(string id)
-          {
-              PRESTAMO pRESTAMO = db.PRESTAMOS.Find(id);
-              pRESTAMO.Estado = 6;
-              db.SaveChanges();
-              return RedirectToAction("Historial");
-          }*/
-        /*
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            EQUIPO_SOLICITADO eQUIPO_SOLICITADO = db.EQUIPO_SOLICITADO.Find(id);
-            db.EQUIPO_SOLICITADO.Remove(eQUIPO_SOLICITADO);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-         */
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
