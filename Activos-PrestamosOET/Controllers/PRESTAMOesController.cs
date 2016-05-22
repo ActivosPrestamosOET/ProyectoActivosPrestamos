a8635a686b4b6e6ac2bea1a6d63a41d090db7abb
cavila31 <cavila31@hotmail.com>
2016-05-22 13:24:33 -0600

    cosillas del PDF

--- Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
+++ Activos-PrestamosOET/Controllers/PRESTAMOesController.cs
@@ -9,7 +9,11 @@ using System;
 using PagedList;
 using System.Globalization;
 using System.Collections.Generic;
-
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.Web;
+using System.IO;
 
 namespace Activos_PrestamosOET.Controllers
 {
@@ -1243,5 +1247,51 @@ namespace Activos_PrestamosOET.Controllers
             return activos_enCat;
     }
 
-}
+        public byte[] GetPDF(string pHTML)
+        {
+            byte[] bPDF = null;
+
+            MemoryStream ms = new MemoryStream();
+            TextReader txtReader = new StringReader(pHTML);
+
+            // 1: create object of a itextsharp document class
+            Document doc = new Document(PageSize.A4, 25, 25, 25, 25);
+
+            // 2: we create a itextsharp pdfwriter that listens to the document and directs a XML-stream to a file
+            PdfWriter oPdfWriter = PdfWriter.GetInstance(doc, ms);
+
+            // 3: we create a worker parse the document
+            HTMLWorker htmlWorker = new HTMLWorker(doc);
+
+            // 4: we open document and start the worker on the document
+            doc.Open();
+            htmlWorker.StartDocument();
+
+            // 5: parse the html into the document
+            htmlWorker.Parse(txtReader);
+
+            // 6: close the document and the worker
+            htmlWorker.EndDocument();
+            htmlWorker.Close();
+            doc.Close();
+
+            bPDF = ms.ToArray();
+
+            return bPDF;
+        }
+
+        public void DownloadPDF()
+        {
+            string HTMLContent = "Hello <b>World</b>";
+
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=" + "PDFfile.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.BinaryWrite(GetPDF(HTMLContent));
+            Response.End();
+        }
+
+
+    }
 }
