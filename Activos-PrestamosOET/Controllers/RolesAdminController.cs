f26f45f8603393e58bcd1ce86a173939c0c831d5
Fabian Rodriguez <farodriguez.49@gmail.com>
2016-06-12 19:11:39 -0600

    Revert "Se quitan las opciones de los roles y solo se deja la opción de ver la info del rol"
    This reverts commit 93158f78d36734966ea2e98b0cf4375e0147975c.

--- Activos-PrestamosOET/Controllers/RolesAdminController.cs
+++ Activos-PrestamosOET/Controllers/RolesAdminController.cs
@@ -84,5 +84,119 @@ namespace Activos_PrestamosOET.Controllers
             ViewBag.UserCount = users.Count();
             return View(role);
         }
+
+        //
+        //// GET: /Roles/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Roles/Create
+        //[HttpPost]
+        //public async Task<ActionResult> Create(RoleViewModel roleViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var role = new IdentityRole(roleViewModel.Name);
+
+        //        // Save the new Description property:
+        //        var roleresult = await RoleManager.CreateAsync(role);
+        //        if (!roleresult.Succeeded)
+        //        {
+        //            ModelState.AddModelError("", roleresult.Errors.First());
+        //            return View();
+        //        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+
+        //
+        // GET: /Roles/Edit/Admin
+    //    public async Task<ActionResult> Edit(string id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        var role = await RoleManager.FindByIdAsync(id);
+    //        if (role == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        RoleViewModel roleModel = new RoleViewModel { Id = role.Id, Name = role.Name };
+
+    //        return View(roleModel);
+    //    }
+
+
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<ActionResult> Edit([Bind(Include = "Name,Id")] RoleViewModel roleModel)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            var role = await RoleManager.FindByIdAsync(roleModel.Id);
+    //            role.Name = roleModel.Name;
+
+    //            await RoleManager.UpdateAsync(role);
+    //            return RedirectToAction("Index");
+    //        }
+    //        return View();
+    //    }
+
+    //    //
+    //    // GET: /Roles/Delete/5
+    //    public async Task<ActionResult> Delete(string id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        var role = await RoleManager.FindByIdAsync(id);
+    //        if (role == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(role);
+    //    }
+
+    //    //
+    //    // POST: /Roles/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<ActionResult> DeleteConfirmed(string id, string deleteUser)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            if (id == null)
+    //            {
+    //                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //            }
+    //            var role = await RoleManager.FindByIdAsync(id);
+    //            if (role == null)
+    //            {
+    //                return HttpNotFound();
+    //            }
+    //            IdentityResult result;
+    //            if (deleteUser != null)
+    //            {
+    //                result = await RoleManager.DeleteAsync(role);
+    //            }
+    //            else
+    //            {
+    //                result = await RoleManager.DeleteAsync(role);
+    //            }
+    //            if (!result.Succeeded)
+    //            {
+    //                ModelState.AddModelError("", result.Errors.First());
+    //                return View();
+    //            }
+    //            return RedirectToAction("Index");
+    //        }
+    //        return View();
+    //    }
     }
 }
